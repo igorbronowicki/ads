b8db90952078182eb7aa72bf88b1dd0215487184
dadr <dadr@ciklum.com>
2014-12-05 01:32:31 +0200

    Task 2- part 1

--- js/index.js
+++ js/index.js
@@ -1,40 +1,41 @@
 console.log('here');
-var AdModel = Backbone.Model.extend({
+var listModel = Backbone.Model.extend({
     defaults: {
-        'title': 'ololo',
-        "description":  "caesar salad",
-        'price': 44
+        'title': 'Boiled Cat',
+        'description':  "eww eq qw eqwfvxcxcvxcv eqw qw eqw eqw",
+        'imageSrc':'img/cat_2.jpg',
+        'price': 543
     }
 });
 
-var AddCollection = Backbone.Collection.extend({
-    model: AdModel
+var listCollection = Backbone.Collection.extend({
+    model: listModel
 });
 
 
 
-var addCollection = new AddCollection([
+var catCollection = new listCollection([
     {
         'title': 'Boiled Cat',
         'description':  "eww eq qw eqwfvxcxcvxcv eqw qw eqw eqw",
-        'imageSrc':'',
+        'imageSrc':'img/cat_2.jpg',
         'price': 543
     },
     {
         'title': 'Fryed Cat',
         'description':  "eww equiouououiouiouiw eqw qw eqw eqw",
-        'image':'',
+        'image':'img/cat_1.jpg',
         'price': 4
     },
     {
         'title': 'Fresh cat with mashrooms',
         'description':  "ewwvvbcvbbcvbcvbcvcvbqw eqw eqw qw eqw eqw",
-        'imageSrc':'',
+        'imageSrc':'img/cat_1.jpg',
         'price': 7
     },
     {
         'title': 'Sweet cat under chocolate',
-        'imageSrc':'',
+        'imageSrc':'img/cat_2.jpg',
         'description':  "werasdsdsdfdsfqw eqw eqw qw eqw eqw",
         'price': 776
     }
@@ -42,23 +43,21 @@ var addCollection = new AddCollection([
 
 
 
-var AdsView = Backbone.View.extend({
+var listView = Backbone.View.extend({
     initialize: function() {
         this.render();
     },
     events: {
         "click .dro"       : "testClick"
     },
-    el: 'body',
-    template: '{{#ads}}<p class="xxx">{{title}}<br>{{description}}<br>{{price}}</p>{{/ads}}',
+    el: '#list',
+    template: $('#tpl-cat-detail').html(),
     render: function() {
-        this.$el.html(Mustache.render(this.template, {
-            ads: addCollection.toJSON()
+        this.$el.html(doT.template(this.template, {
+            cats: catCollection.toJSON()
         }));
     },
     testClick: function() {
         alert('testClick');
     }
 });
-
-AdsView.init();
