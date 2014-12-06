74ac684a057959063ff07493d174ac1f09f223ff
dadr <dadr@ciklum.com>
2014-12-07 00:55:16 +0200

    Task 2- part 1

--- js/index.js
+++ js/index.js
@@ -1,63 +1,68 @@
-console.log('here');
-var listModel = Backbone.Model.extend({
-    defaults: {
-        'title': 'Boiled Cat',
-        'description':  "eww eq qw eqwfvxcxcvxcv eqw qw eqw eqw",
-        'imageSrc':'img/cat_2.jpg',
-        'price': 543
-    }
-});
 
-var listCollection = Backbone.Collection.extend({
-    model: listModel
-});
+    console.log('here');
+    var listModel = Backbone.Model.extend({
+        defaults: {
+            'title': 'Boiled Cat',
+            'description':  "eww eq qw eqwfvxcxcvxcv eqw qw eqw eqw",
+            'imageSrc':'img/cat_1.jpg',
+            'price': 543
+        }
+    });
 
-
-
-var catCollection = new listCollection([
-    {
-        'title': 'Boiled Cat',
-        'description':  "eww eq qw eqwfvxcxcvxcv eqw qw eqw eqw",
-        'imageSrc':'img/cat_2.jpg',
-        'price': 543
-    },
-    {
-        'title': 'Fryed Cat',
-        'description':  "eww equiouououiouiouiw eqw qw eqw eqw",
-        'image':'img/cat_1.jpg',
-        'price': 4
-    },
-    {
-        'title': 'Fresh cat with mashrooms',
-        'description':  "ewwvvbcvbbcvbcvbcvcvbqw eqw eqw qw eqw eqw",
-        'imageSrc':'img/cat_1.jpg',
-        'price': 7
-    },
-    {
-        'title': 'Sweet cat under chocolate',
-        'imageSrc':'img/cat_2.jpg',
-        'description':  "werasdsdsdfdsfqw eqw eqw qw eqw eqw",
-        'price': 776
-    }
-]);
+    var listCollection = Backbone.Collection.extend({
+        model: listModel
+    });
 
 
 
-var listView = Backbone.View.extend({
-    initialize: function() {
-        this.render();
-    },
-    events: {
-        "click .dro"       : "testClick"
-    },
-    el: $('#list'),
-    template: $('#tpl-cat-detail').html(),
-    render: function() {
-        this.$el.html(doT.template(this.template, {
-            cats: catCollection.toJSON()
-        }));
-    },
-    testClick: function() {
-        alert('testClick');
-    }
-});
+    var catCollection = new listCollection([
+        {
+            'title': 'Boiled Cat with cheese',
+            'description':  "eww eq qw eqwfvxcxcvxcv eqw qw eqw eqw",
+            'imageSrc':'img/cat_1.jpg',
+            'price': 543
+        },
+        {
+            'title': 'Fryed Cat',
+            'description':  "eww equiouououiouiouiw eqw qw eqw eqw",
+            'imageSrc':'img/cat_2.jpg',
+            'price': 4
+        },
+        {
+            'title': 'Fresh cat with mashrooms',
+            'description':  "ewwvvbcvbbcvbcvbcvcvbqw eqw eqw qw eqw eqw",
+            'imageSrc':'img/cat_1.jpg',
+            'price': 7
+        },
+        {
+            'title': 'Sweet cat under chocolate',
+            'imageSrc':'img/cat_2.jpg',
+            'description':  "werasdsdsdfdsfqw eqw eqw qw eqw eqw",
+            'price': 776
+        }
+    ]);
+
+
+
+    var listView = Backbone.View.extend({
+        initialize: function() {
+            this.render();
+        },
+        events: {
+            "click .dro": "testClick"
+        },
+        el:'#list',
+        template: doT.template($('#tpl-cat-detail').html()),
+        render: function() {
+            this.$el.html(this.template, {
+                cats: catCollection.toJSON()
+            });
+            return this;
+        },
+        testClick: function() {
+            alert('testClick');
+        }
+    });
+
+    var App = new listView;
+
