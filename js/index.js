011eca70587121cdaba34918c76c43d57af109cf
dadr <dadr@ciklum.com>
2014-12-09 00:40:17 +0200

    Task 3

--- js/index.js
+++ js/index.js
@@ -1,7 +1,8 @@
-
+$(document).on('ready', function(){
     console.log('here');
     var listModel = Backbone.Model.extend({
         defaults: {
+            'id':0,
             'title': 'Boiled Cat',
             'description':  "eww eq qw eqwfvxcxcvxcv eqw qw eqw eqw",
             'imageSrc':'img/cat_1.jpg',
@@ -17,24 +18,28 @@
 
     var catCollection = new listCollection([
         {
+            'id':1,
             'title': 'Cat Chips',
             'description':  "eww equiouououiouiouiw eqw qw eqw eqw",
             'imageSrc':'img/cat_2.jpg',
             'price': 4
         },
         {
+            'id':2,
             'title': 'Fresh cat with mashrooms',
             'description':  "ewwvvbcvbbcvbcvbcvcvbqw eqw eqw qw eqw eqw",
             'imageSrc':'img/cat_3.jpg',
             'price': 7
         },
         {
+            'id': 3,
             'title': 'Sweet cat salad with tomatos',
             'imageSrc':'img/cat_1.jpg',
             'description':  "werasdsdsdfdsfqw eqw eqw qw eqw eqw",
             'price': 776
         },
         {
+            'id':4,
             'title': 'Microwaved Cat with cheese',
             'description':  "eww eq qw eqwfvxcxcvxcv eqw qw eqw eqw",
             'imageSrc':'img/cat_4.jpg',
@@ -50,22 +55,26 @@
             this.render();
         },
         events: {
-            "click .dro": "testClick"
+            "click .close": "removeItem"
         },
         el:'#list',
-        data:catCollection.toJSON(),
-        template: doT.template($('#tpl-cat-detail').html()),
+        template: $('#tpl-cat-detail').html(),
         render: function() {
-            var tmpl = '';
-            for (var i = 0; i < this.data.length; i++) {
-                tmpl += this.template(this.data[i]);
-            }
-            this.$el.html(tmpl);
+                this.$el.html(doT.template(this.template)({
+                    ads: catCollection.toJSON()
+                }));
         },
-        testClick: function() {
-            alert('testClick');
+        removeItem: function(e) {
+
+            var item = $(e.target).closest('.list-item');
+            var model = catCollection.get(item.data('item-id'));
+
+            item.fadeOut();
+            catCollection.remove(model);
         }
     });
 
+
     var App = new listView;
+});
 
